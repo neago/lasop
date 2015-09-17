024533bf673b24959e894e12d9a77753599dd85a
Jonas Neergaard-Nielsen <j@neer.dk>
2015-09-17 07:05:47 +0200

    Finished abcd and mode size functions.

--- opo.py
+++ opo.py
@@ -16,11 +16,11 @@ class BowtieOPO:
     """
     Implements bowtie geometry.
     """
-    def __init__(self, Lc, L1, L2, R1, R2=np.inf, folding_angle=5*np.pi/180, nc=1.8,
-                 fixed_first='L2', fixed_second='folding_angle'):
+    def __init__(self, Lc, L1, L, R1, R2=np.inf, folding_angle=5*np.pi/180, nc=1.8,
+                 fixed_first='L', fixed_second='folding_angle'):
         self._Lc = Lc
         self._L1 = L1
-        self._L2 = L2
+        self._L = L
         self._R1 = R1
         self._R2 = R2
         self._folding_angle = folding_angle
@@ -28,8 +28,8 @@ class BowtieOPO:
         self.fixed_first = fixed_first
         self.fixed_second = fixed_second
 
+        self.elements, self.M, self.q0 = {}, {}, {}
         self.update_geometry()
-        self.update_mode()
 
 
     @property
@@ -152,8 +152,11 @@ class BowtieOPO:
                 self._L2 = 2 * self._L12 * np.cos(self.folding_angle) - self.L1
             self._L = self.L1 + self.L2 + 2 * self._L12 + (self.nc - 1) * self.Lc
 
+        for dir in ['h', 'v']:
+            for d, v in zip([self.elements, self.M, self.q0], self.get_abcd(dir)):
+                d[dir] = v
 
-    def update_mode(self):
+    def update_abcd_old(self):
         elements_z = [self.Lc / 2,
                       self.L1 / 2,
                       self.L1 / 2 + self._L12,
@@ -166,7 +169,80 @@ class BowtieOPO:
                            abcd.Mmirror(-self.R2 * np.cos(self.folding_angle)),
                            abcd.Mmirror(-self.R1 * np.cos(self.folding_angle)),
                            abcd.Minterface(1, self.nc)]
-        self.elements_horz = zip(elements_z, elements_M_horz)
+        elements_M_vert = [abcd.Minterface(self.nc, 1),
+                           abcd.Mmirror(-self.R1 / np.cos(self.folding_angle)),
+                           abcd.Mmirror(-self.R2 / np.cos(self.folding_angle)),
+                           abcd.Mmirror(-self.R2 / np.cos(self.folding_angle)),
+                           abcd.Mmirror(-self.R1 / np.cos(self.folding_angle)),
+                           abcd.Minterface(1, self.nc)]
+        elements_horz = zip(elements_z, elements_M_horz)
+        elements_vert = zip(elements_z, elements_M_vert)
+
+        M_horz = np.asmatrix(np.identity(2))
+        M_vert = np.asmatrix(np.identity(2))
+        lastpos = 0
+        for eh, ev in zip(elements_horz, elements_vert):
+            M_horz = eh[1] * abcd.Mprop(eh[0] - lastpos) * M_horz
+            M_vert = ev[1] * abcd.Mprop(ev[0] - lastpos) * M_vert
+            lastpos = eh[0]
+        M_horz = abcd.Mprop(self.Lc / 2) * M_horz
+        M_vert = abcd.Mprop(self.Lc / 2) * M_vert
+
+        q0_horz = find_stable_mode(M_horz, True)
+        q0_vert = find_stable_mode(M_vert, True)
+
+        return ((elements_horz, M_horz, q0_horz), (elements_vert, M_vert, q0_vert))
+
+    def get_abcd(self, dir='h'):
+        if dir == 'h':
+            R1 = self.R1 * np.cos(self.folding_angle)
+            R2 = self.R2 * np.cos(self.folding_angle)
+        else:
+            R1 = self.R1 / np.cos(self.folding_angle)
+            R2 = self.R2 / np.cos(self.folding_angle)
+
+        elements_z = [self.Lc / 2,
+                      self.L1 / 2,
+                      self.L1 / 2 + self._L12,
+                      self.L1 / 2 + self._L12 + self.L2,
+                      self.L1 / 2 + 2 * self._L12 + self.L2,
+                      self.L1 + 2 * self._L12 + self.L2 - self.Lc / 2]
+        elements_M = [abcd.Minterface(self.nc, 1),
+                      abcd.Mmirror(-R1),
+                      abcd.Mmirror(-R2),
+                      abcd.Mmirror(-R2),
+                      abcd.Mmirror(-R1),
+                      abcd.Minterface(1, self.nc)]
+        elements = [(ez, eM) for (ez, eM) in zip(elements_z, elements_M)]
+
+        M = np.asmatrix(np.identity(2))
+        lastpos = 0
+        for e in elements:
+            M = e[1] * abcd.Mprop(e[0] - lastpos) * M
+            lastpos = e[0]
+        M = abcd.Mprop(self.Lc / 2) * M
+
+        q0 = find_stable_mode(M, True)
+
+        return elements, M, q0
+
+    def mode_waist(self, dir='h', waist=1):
+        if waist == 1:
+            return abcd.q2w(self.q0[dir], self.nc)
+        else:
+            q = abcd.qpropagate(0, self.q0[dir], self.elements[dir],
+                                self.L1 / 2 + self._L12 + self.L2 / 2)
+            return abcd.q2w(q)
+
+    def mode_width_at(self, z, dir='h'):
+        """
+        Get mode radius at arbitrary location.
+        Note: this function does not yet take into account the crystal
+        refractive index.
+        """
+        q = abcd.qpropagate(0, self.q0[dir], self.elements[dir], z)
+        return abcd.q2w(q)
+
         #self.M_horz =
 
 
@@ -206,3 +282,36 @@ class BowtieOPO:
         mode : Mode
             Mode at given location.
         """
+
+def find_stable_mode(M, quiet=False):
+    """
+    Find the stable mode for a cavity with system matrix M.
+
+    Parameters
+    ----------
+    M : matrix (2x2)
+        ABCD matrix for a cavity return trip
+
+    Returns
+    -------
+    q : complex
+        q-parameter of the stable cavity mode.
+    """
+    A, B, C, D = M[0,0], M[0,1], M[1,0], M[1,1]
+    det = np.complex((A-D)**2 + 4*B*C)
+
+    solutions = ((A-D + np.sqrt(det)) / (2*C),
+                 (A-D - np.sqrt(det)) / (2*C))
+
+    if solutions[0].imag > 0:
+        return solutions[0]
+    elif solutions[1].imag > 0:
+        return solutions[1]
+    else:
+        if not quiet:
+            print('No solutions')
+        #print('No solutions, trying an eps difference...')
+        #eps = np.finfo(np.float).eps
+        #eps = .1
+        #return find_stable_mode(M + np.diag([eps, 0]))
+        return None
