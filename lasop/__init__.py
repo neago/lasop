aab2a9bf682e20c0400ec7ffcd97ee2cae669b5c
Jonas Neergaard-Nielsen <j@neer.dk>
2017-01-12 12:48:48 +0100

    Change structure to package

---
+++ lasop/__init__.py
@@ -0,0 +1,4 @@
+import abcd
+import beamprofile
+import opo
+import spotsize
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='lasop',
+      version='0.1',
+      description='Lasop - tools for the quantum optics lab',
+      author='Jonas S. Neergaard-Nielsen',
+      author_email='j@neer.dk',
+      packages=['lasop'],
+     )
